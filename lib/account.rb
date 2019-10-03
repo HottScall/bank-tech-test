cdbeb9d004880d79f26441debbab0f090f0e0bca
Scott <Scotthall1983@hotmail.com>
2019-10-03 22:20:48 +0100

    Added instance variable for date method, but need to look at the seperation of concerns for transaction history

--- lib/account.rb
+++ lib/account.rb
@@ -1,9 +1,11 @@
 class Account
 
-  attr_reader :balance
+  attr_reader :balance, :date
 
   def initialize
     @balance = 0
+    p @date
+    @date = ["10/01/2012", "13/01/2012" "14/01/2012"]
   end
 
   def deposit(amount)
@@ -15,7 +17,7 @@ class Account
   end
 
   def date
-    "14/01/2012"
+
   end
 
 
